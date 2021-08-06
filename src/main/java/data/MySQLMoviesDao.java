6b464a84acc4e43cd20e9c66dd236db492885c28
shawcorey <shaw.corey@gmail.com>
2021-08-06 16:02:39 -0500

    updated files

--- src/main/java/data/MySQLMoviesDao.java
+++ src/main/java/data/MySQLMoviesDao.java
@@ -1,9 +1,36 @@
 package data;
 
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
-public class MySQLMoviesDao implements MoviesDao{
+public class MySQLMoviesDao implements MoviesDao {
+
+    private Connection connection = null;
+
+    public MySQLMoviesDao(Config config) {
+
+        try {
+            DriverManager.registerDriver(new Driver());
+
+            this.connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        }
+       catch(SQLException e){
+                throw new RuntimeException("Error connnecting to the database", e);
+            }
+
+
+    }
+
     @Override
     public List<Movie> all() throws SQLException {
         return null;
@@ -18,10 +45,41 @@ public class MySQLMoviesDao implements MoviesDao{
     public void insert(Movie movie) {
 
     }
-
     @Override
     public void insertAll(Movie[] movies) throws SQLException {
 
+        // Build sql template
+        StringBuilder sql = new StringBuilder("INSERT INTO movies (" +
+                "id, title, year, director, actors, imdbId, poster, genre, plot) " +
+                "VALUES ");
+
+
+        // Add a interpolation template for each element in movies list
+        sql.append("(?, ?, ?, ?, ?, ?, ?, ?, ?), ".repeat(movies.length));
+
+        // Create a new String and take off the last comma and whitespace
+        sql = new StringBuilder(sql.substring(0, sql.length() - 2));
+
+        // Use the sql string to create a prepared statement
+        PreparedStatement statement = connection.prepareStatement(sql.toString());
+
+        // Add each movie to the prepared statement using the index of each sql param: '?'
+        // This is done by using a counter
+        // You could use a for loop to do this as well and use its incrementor
+        int counter = 0;
+        for (Movie movie : movies) {
+            statement.setInt((counter * 9) + 1, movie.getId());
+            statement.setString((counter * 9) + 2, movie.getTitle());
+            statement.setString((counter * 9) + 3, movie.getYear());
+            statement.setString((counter * 9) + 4, movie.getDirector());
+            statement.setString((counter * 9) + 5, movie.getActors());
+            statement.setString((counter * 9) + 6, movie.getImdbID());
+            statement.setString((counter * 9) + 7, movie.getPoster());
+            statement.setString((counter * 9) + 8, movie.getGenre());
+            statement.setString((counter * 9) + 9, movie.getPlot());
+            counter++;
+        }
+        statement.executeUpdate();
     }
 
     @Override
